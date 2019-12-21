bec460f5409e8053c28d082aa62257fbf3d1764d
Hamid <mr.hamidsamir@gmail.com>
2019-12-21 15:18:01 +0330

    test

---
+++ logutil/src/main/java/artmis/org/logutil/LogMe.java
@@ -0,0 +1,17 @@
+package artmis.org.logutil;
+
+import android.util.Log;
+
+public class LogMe {
+
+
+    public static  void show(String value){
+
+        Log.i("hsamir","show the value: " + value);
+
+    }
+
+
+
+
+}
